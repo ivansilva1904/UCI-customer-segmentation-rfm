13bc861be30a649bf65cbb2d86b359e2cd63e181
ivansilva1904 <ivan.silva04599@gmail.com>
2026-06-20 19:04:26 -0300

    just saving the changes in group_segmentation.py to backtrack to rfm_kmeans.py

--- scripts/group_segmentation.py
+++ scripts/group_segmentation.py
@@ -3,16 +3,27 @@ import pandas as pd
 import os
 
 directory = os.path.dirname(os.path.abspath(__file__))
-df = pd.read_excel(os.path.join(directory, '..', 'data', '4-Online Retail_RFM KMeans.xlsx'))
+df = pd.read_excel(os.path.join(directory, '..', 'data', '5-Online Retail_RFM KMeans.xlsx'))
 
-group_df = pd.DataFrame()
+#no log
+nolog_df = pd.DataFrame()
 
-group_df['Recency mean'] = df.groupby('Cluster log')['Recency'].mean()
-group_df['Frequency mean'] = df.groupby('Cluster log')['Frequency'].mean()
-group_df['Monetary mean'] = df.groupby('Cluster log')['Monetary'].mean()
+nolog_df['Recency mean'] = df.groupby('No log cluster')['Recency'].mean()
+nolog_df['Frequency mean'] = df.groupby('No log cluster')['Frequency'].mean()
+nolog_df['Monetary mean'] = df.groupby('No log cluster')['Monetary'].mean()
 
-group_df = group_df.reset_index()
+nolog_df = nolog_df.reset_index()
 
-print(group_df)
-print(f"\nThe groups are:\nCluster 1: VIP clients\nCluster 0: New clients\nCluster 2: At risk clients\nCluster 3: Lost clients")
+#log
+log_df = pd.DataFrame()
+
+log_df['Recency mean'] = df.groupby('Log cluster')['Recency'].mean()
+log_df['Frequency mean'] = df.groupby('Log cluster')['Frequency'].mean()
+log_df['Monetary mean'] = df.groupby('Log cluster')['Monetary'].mean()
+
+log_df = log_df.reset_index()
+
+print(nolog_df)
+print("\n")
+print(log_df)
 
