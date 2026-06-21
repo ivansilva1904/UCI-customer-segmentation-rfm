0924a358ed1ff618fc6adfa22697f665747b62fc
ivansilva1904 <ivan.silva04599@gmail.com>
2026-06-21 01:04:47 -0300

    just added a print to group_segmentation.py to show the groups

--- scripts/group_segmentation.py
+++ scripts/group_segmentation.py
@@ -5,25 +5,14 @@ import os
 directory = os.path.dirname(os.path.abspath(__file__))
 df = pd.read_excel(os.path.join(directory, '..', 'data', '5-Online Retail_RFM KMeans.xlsx'))
 
-#no log
-nolog_df = pd.DataFrame()
+rfm_df = pd.DataFrame()
 
-nolog_df['Recency mean'] = df.groupby('No log cluster')['Recency'].mean()
-nolog_df['Frequency mean'] = df.groupby('No log cluster')['Frequency'].mean()
-nolog_df['Monetary mean'] = df.groupby('No log cluster')['Monetary'].mean()
+rfm_df['Recency mean'] = df.groupby('Cluster')['Recency'].mean()
+rfm_df['Frequency mean'] = df.groupby('Cluster')['Frequency'].mean()
+rfm_df['Monetary mean'] = df.groupby('Cluster')['Monetary'].mean()
 
-nolog_df = nolog_df.reset_index()
+rfm_df = rfm_df.reset_index()
 
-#log
-log_df = pd.DataFrame()
-
-log_df['Recency mean'] = df.groupby('Log cluster')['Recency'].mean()
-log_df['Frequency mean'] = df.groupby('Log cluster')['Frequency'].mean()
-log_df['Monetary mean'] = df.groupby('Log cluster')['Monetary'].mean()
-
-log_df = log_df.reset_index()
-
-print(nolog_df)
-print("\n")
-print(log_df)
+print(rfm_df)
+print(f"The groups are:\nCluster 1: Loyal clients\nCluster 0: New clients\nCluster 2: At risk clients\nCluster 3: Inactive clients")
 
