8e86d78bea6a8c535fa063934f7d53cedea07ef8
ivansilva1904 <ivan.silva04599@gmail.com>
2026-06-21 01:19:00 -0300

    modified main.py to accommodate to the changes in the pipeline. i run it so the data files also 'updated'. not really

--- scripts/main.py
+++ scripts/main.py
@@ -5,17 +5,21 @@ import sys
 directory = os.path.dirname(os.path.abspath(__file__))
 
 data_cleaning_path = os.path.join(directory, 'data_cleaning.py')
-rfm_heuristics_path = os.path.join(directory, 'rfm_heuristics.py')
-rfm_kmeans_path = os.path.join(directory, 'rfm_kmeans.py')
+rfm_table_path = os.path.join(directory, 'rfm_table.py')
+rfm_heuristic_path = os.path.join(directory, 'rfm_heuristic_method.py')
+rfm_kmeans_path = os.path.join(directory, 'rfm_kmeans_method.py')
 group_segmentation = os.path.join(directory, 'group_segmentation.py')
 
 print(f"----------Starting data_cleaning.py----------\n")
 subprocess.run([sys.executable, data_cleaning_path], check=True)
 
-print(f"\n----------Starting rfm_heuristics.py----------\n")
-subprocess.run([sys.executable, rfm_heuristics_path], check=True)
+print(f"\n----------Starting rfm_table.py----------\n")
+subprocess.run([sys.executable, rfm_table_path], check=True)
 
-print(f"\n----------Starting rfm_kmeans.py----------\n")
+print(f"\n----------Starting rfm_heuristic_method.py----------\n")
+subprocess.run([sys.executable, rfm_heuristic_path], check=True)
+
+print(f"\n----------Starting rfm_kmeans_method.py----------\n")
 subprocess.run([sys.executable, rfm_kmeans_path], check=True)
 
 print(f"\n----------Starting group_segmentation.py----------\n")
